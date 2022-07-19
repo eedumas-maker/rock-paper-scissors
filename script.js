fcc3cdab132a0e602b88f5ab4b8b8bf74e62312f
Evan Dumas <eedumas@gmail.com>
2022-07-18 17:16:13 -0700

    Add function for computer choice

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ Let's write out the pseudo code before I actually write any code,
 see what problems I need to solve and in what order
 
 */
+
+function getComputerChoice() {
+    let choiceInt = Math.floor(Math.random()*3)+1;
+    console.log(choiceInt);
+    let choice = "";
+    
+    switch (choiceInt) {
+        case 1: choice = "Rock";
+        break;
+        case 2: choice = "Paper";
+        break;
+        case 3: choice = "Scissors";
+        break;
+        default: choice = "That's not a choice";
+        
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
