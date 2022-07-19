564d6caae2e57bb8a5d38d0d073c5d979f21485e
Evan Dumas <eedumas@gmail.com>
2022-07-18 20:18:07 -0700

    Add play round and game functions

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ see what problems I need to solve and in what order
 
 */
 
+let playerChoice = "";
+
 function getComputerChoice() {
     let choiceInt = Math.floor(Math.random()*3)+1;
-    console.log(choiceInt);
+
     let choice = "";
     
     switch (choiceInt) {
@@ -19,9 +21,84 @@ function getComputerChoice() {
         case 3: choice = "Scissors";
         break;
         default: choice = "That's not a choice";
-        
     }
     return choice;
 }
 
-console.log(getComputerChoice());
+function playerSelection(playerChoice) {
+    return playerChoice.toLowerCase();
+}
+
+function playGame(pChoice, cChoice){
+    // get first letter of each choice, easier to compare
+    // this also seems redundant, as we're already passing in playerChoice....
+
+    cChoice = cChoice.toLowerCase(); // make it match the already lower case player choice
+
+    let pAbrv = pChoice.slice(0,1); //short for player abbreviation
+    let cAbrv = cChoice.slice(0,1).toLowerCase();
+
+    let results;
+
+    console.log("playerChoice is " + pChoice);
+
+    console.log("compChoice is " + cChoice);
+
+    if (pAbrv === cAbrv) {
+        results = "It's a draw!"
+    }
+    else if ((pAbrv === 'r' && cAbrv === 's') 
+            || (pAbrv === 'p' && cAbrv === 'r') 
+            || (pAbrv === 's' && cAbrv === 'p')){
+        results = "You win! " + pChoice + " beats " + cChoice + "."
+            }
+    else {
+       results = "You lose! " + cChoice + " beats " + pChoice + "."
+    }
+
+    return results;
+
+    }
+
+function game() {
+
+    let pScore = 0;
+    let cScore = 0;
+
+    for (let i = 0; i < 5; i++)
+    {
+        playerChoice = prompt("Round " + i + 1 + " Choose your weapon: Rock, Paper, or Scissors");
+        playGame(playerChoice, getComputerChoice()).slice(5,5) === "w" ? pScore++ : cScore++;
+    }
+    alert("The final score: Player " + pScore + " Computer " + cScore);
+}
+
+game();
+
+
+
+    // ok maybe shrinking to letters wasn't good? nah
+    // how to separate the win/lose/tie conditions?
+
+    // two main branches, you win or you lose or you draw
+    // could be a switch statement
+
+    // hmm, gotta pee and think
+
+    /*
+    So there's three conditions, you win you lose or you draw
+
+    You draw is easy: your answer = comp answer
+    You win: p > r, r > s, s > p
+    You lose: r < p, s < r, p < s,
+
+    So if statements
+    if tie, draw
+    if win or win or win
+       you win
+    if lose or lose or lose
+       you lose
+
+    */
+
+
