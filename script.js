5e840134f4621f9868a219ca62f6c966d841ee1b
Evan Dumas <eedumas@gmail.com>
2022-07-31 18:27:40 -0700

    Refactor code to add buttons

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 console.log("Hello from the script.js file");
 
-/*
-Let's write out the pseudo code before I actually write any code, 
-see what problems I need to solve and in what order
-
-*/
 
 let playerChoice = "";
 
@@ -25,15 +20,12 @@ function getComputerChoice() {
     return choice;
 }
 
-function playerSelection(playerChoice) {
-    return playerChoice.toLowerCase();
-}
 
 function playGame(pChoice, cChoice){
     // get first letter of each choice, easier to compare
     // this also seems redundant, as we're already passing in playerChoice....
 
-    cChoice = cChoice.toLowerCase(); // make it match the already lower case player choice
+    // cChoice = cChoice.toLowerCase(); // make it match the already lower case player choice
 
     let pAbrv = pChoice.slice(0,1); //short for player abbreviation
     let cAbrv = cChoice.slice(0,1).toLowerCase();
@@ -56,72 +48,20 @@ function playGame(pChoice, cChoice){
        results = "You lose! " + cChoice + " beats " + pChoice + "."
     }
 
-    return results;
+    return alert(results);
 
     }
 
-function game() {
+const rockButt = document.querySelector('#rock');
+const paperButt = document.querySelector('#paper');
+const scissorsButt = document.querySelector('#scissors');
 
-    let pScore = 0;
-    let cScore = 0;
-    let roundScore = "";
-
-    for (let i = 1; i < 6; i++)
-    {
-        playerChoice = prompt("Round " + i + " Choose your weapon: Rock, Paper, or Scissors");
-        roundScore = playGame(playerChoice, getComputerChoice());
-
-        alert(roundScore);
-
-        if (roundScore.slice(4,5) === "w")
-        {
-            pScore++;
-        }
-        else if (roundScore.slice(4,5) === "l")
-        {
-            cScore++;
-        }
-        // do i need to make a draw condition? nothing should happen if it's an "a"
-     
-
-        console.log("roundScore: " + roundScore);
-        console.log("roundScore slice: " + roundScore.slice(4,5));
-
-        console.log("pScore: " + pScore);
-        console.log("cScore: " + cScore);
-    }
-    alert("The final score: Player " + pScore + " Computer " + cScore);
-}
-
-game();
-
-// Well it works, but the substring answer is clumsy
+console.log(rockButt); // why is that returning null? Ha, had to defer it!
 
 
-// 1st pass pseudo code below
+rockButt.addEventListener('click', () => playGame('rock',getComputerChoice()));
 
-    // ok maybe shrinking to letters wasn't good? nah
-    // how to separate the win/lose/tie conditions?
+paperButt.addEventListener('click', () => playGame('paper',getComputerChoice()));
 
-    // two main branches, you win or you lose or you draw
-    // could be a switch statement
-
-    // hmm, gotta pee and think
-
-    /*
-    So there's three conditions, you win you lose or you draw
-
-    You draw is easy: your answer = comp answer
-    You win: p > r, r > s, s > p
-    You lose: r < p, s < r, p < s,
-
-    So if statements
-    if tie, draw
-    if win or win or win
-       you win
-    if lose or lose or lose
-       you lose
-
-    */
-
+scissorsButt.addEventListener('click', () => playGame('scissors',getComputerChoice()));
 
